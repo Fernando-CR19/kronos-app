6233a3303f29adb348c92b454a9ec0650c183cd8
Fernando <fernandocr019@gmail.com>
2026-03-20 20:35:33 -0300

    feat: add user table on drift

--- lib/database/tables/users_table.dart
+++ lib/database/tables/users_table.dart
@@ -5,8 +5,15 @@ class Users extends Table {
   String get tableName => 'users';
 
   IntColumn get id => integer().autoIncrement()();
+  TextColumn get uuid => text().unique()();
+  DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
+  DateTimeColumn get updatedAt => dateTime().withDefault(currentDateAndTime)();
+  BoolColumn get active => boolean().withDefault(const Constant(true))();
+  BoolColumn get deleted => boolean().withDefault(const Constant(false))();
 
   TextColumn get name => text().nullable()();
-
+  TextColumn get username => text()();
   TextColumn get email => text().unique()();
+  TextColumn get phone => text().nullable()();
+  TextColumn get chatId => text().nullable()();
 }
